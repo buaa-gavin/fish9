19f125584812d8560b7d9a93e2e176554f9fbe54
xie <601862884@qq.com>
2022-07-04 23:26:52 +0800

    use api

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -100,20 +100,13 @@ import _axios from '@/plugins/axios'
 
       },
       generate: function() {
-        // _axios.post('/article/generate', {
-        //   'content': 'helo'
-        // }).then(function (response) {
-        //   console.log('res>>>' + response)
-        //   this.title = response.data.summary
-        //   this.abstract = response.data.abstract
-        // })
+        _axios.post('/article/generate', {
+          'content': 'helo'
+        }).then((response) => {
+          this.title = response.data.title
+          this.abstract = response.data.summary
+        })
         
-        let fake_data = {
-          "title": "tall old man",
-          "abstract": "A rural guy came to the city to look for a job, finally saw through the colorful world, and finally chose the story of the lawyer profession."
-        }
-        this.title = fake_data.title
-        this.abstract = fake_data.abstract
       }
     }
   }
