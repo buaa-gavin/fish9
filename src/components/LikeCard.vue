cff09759745dc936ece9016f681f333576ec9c22
buaa-gavin <3312321558@qq.com>
2022-07-06 02:08:33 +0800

    add likecard component

---
+++ src/components/LikeCard.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-container>
+    <template>
+      <v-card class="mx-auto" max-width="344">
+        <v-card-subtitle> time </v-card-subtitle>
+
+        <v-card-title> {{ title }} </v-card-title>
+
+        <v-card-subtitle> {{ abstract }} </v-card-subtitle>
+
+        <v-card-actions>
+          <v-dialog v-model="dialog" width="600px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="primary" dark v-bind="attrs" v-on="on"> edit </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">修改内容</span>
+              </v-card-title>
+              <v-textarea label="标题" v-model="title"></v-textarea>
+              <v-textarea label="摘要" v-model="abstract"></v-textarea>
+              <v-textarea label="正文" v-model="content"></v-textarea>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue" text @click="save">save</v-btn>
+                <v-btn color="grey" text @click="dialog = false">cancel</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+
+          <v-spacer></v-spacer>
+
+          <v-btn text @click="show = !show">
+            <v-subtitle>{{ show ? "收起全文" : "展开全文" }}</v-subtitle>
+          </v-btn>
+        </v-card-actions>
+
+        <v-expand-transition>
+          <div v-show="show">
+            <v-divider></v-divider>
+
+            <v-card-text> {{ content }} </v-card-text>
+          </div>
+        </v-expand-transition>
+      </v-card>
+    </template>
+  </v-container>
+</template>
+
+<script>
+import _axios from "@/plugins/axios";
+export default {
+  name: "LikeCard",
+
+  data: () => ({
+    show: false,
+    dialog: false,
+    title: "标题哦",
+    abstract: "摘要哦",
+    content: "正文哦",
+  }),
+  watch: {
+    story: function (newInput) {
+      console.log("new>>>>" + newInput);
+    },
+  },
+  methods: {
+    copytoclipboard: function () {
+      let copyData = "title: " + this.title + "\n" + "abstract: " + this.abstract + "\n" + "body: " + this.body + "\n";
+      navigator.clipboard.writeText(copyData);
+    },
+    save: function () {
+      console.log(this.title);
+      console.log(this.abstract);
+      console.log(this.content);
+    },
+    generate: function () {
+      (this.loading = true),
+        _axios
+          .post("/article/generate", {
+            content: this.story,
+          })
+          .then((response) => {
+            this.title = response.data.title;
+            this.abstract = response.data.summary;
+            this.loading = false;
+          });
+    },
+  },
+};
+</script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <like-card/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import LikeCard from '../components/LikeCard.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    LikeCard,
+  }
+}
+</script>
