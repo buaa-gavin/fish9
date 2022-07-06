7ce2b0fbacf467d84b533678c518724f9ef45041
buaa-gavin <3312321558@qq.com>
2022-07-06 23:12:49 +0800

    head modify

--- src/components/UserStatus.vue
+++ src/components/UserStatus.vue
@@ -1,58 +1,75 @@
 <template>
   <div>
-    <v-btn v-if="!isLogin"
-      text
-      @click="jump2Login"
-    >
+    <v-btn v-if="!isLogin" text @click="jump2Login">
       <span class="mr-2">login</span>
     </v-btn>
     <div v-else>
-        <v-btn text> {{ username }} </v-btn>
-        <v-btn icon color="yellow" @click="jump2Favorite">
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text @click="jump2Note" v-bind="attrs" v-on="on"> {{ username }} </v-btn>
+        </template>
+        <span>创作中心</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon color="white" @click="jump2Favorite" v-bind="attrs" v-on="on">
             <v-icon>mdi-star</v-icon>
-        </v-btn>
-        <v-btn icon color="white" @click="logout">
+          </v-btn>
+        </template>
+        <span>收藏</span>
+      </v-tooltip>
+
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon color="white" @click="logout" v-bind="attrs" v-on="on">
             <v-icon>mdi-logout</v-icon>
-        </v-btn>
+          </v-btn>
+        </template>
+        <span>退出登录</span>
+      </v-tooltip>
     </div>
   </div>
 </template>
 
 <script>
-import router from '@/router'
+import router from "@/router";
 
-export default ({
-    name: 'UserStatus',
-    data: () => ({
-        username: 'mike',
-        isLogin: false,
-    }),
-    methods: {
-        jump2Login: () => {
-            if (router.currentRoute.name !== 'login') {
-                router.push('/login')
-            }
-        },
-        jump2Favorite: () => {
-            if (router.currentRoute.name != 'favorite') {
-                router.push('/favorite')
-            }
-        },
-        logout: function(){
-            this.isLogin = false
-            localStorage.removeItem('TOKEN')
-            router.push('/login')
-        }
+export default {
+  name: "UserStatus",
+  data: () => ({
+    username: "mike",
+    isLogin: false,
+  }),
+  methods: {
+    jump2Note: () => {
+      if (router.currentRoute.name != "home") {
+        router.push("/");
+      }
     },
-    mounted() {
-        this.$bus.$on('loginSuccessful', () => {
-            this.isLogin = true
-            this.username = localStorage.getItem('username')
-        })
+    jump2Login: () => {
+      if (router.currentRoute.name !== "login") {
+        router.push("/login");
+      }
     },
-    beforeDestory() {
-        this.$bus.$off('loginSuccessful')
-    }
-})
-
+    jump2Favorite: () => {
+      if (router.currentRoute.name != "favorite") {
+        router.push("/favorite");
+      }
+    },
+    logout: function () {
+      this.isLogin = false;
+      localStorage.removeItem("TOKEN");
+      router.push("/login");
+    },
+  },
+  mounted() {
+    this.$bus.$on("loginSuccessful", () => {
+      this.isLogin = true;
+      this.username = localStorage.getItem("username");
+    });
+  },
+  beforeDestory() {
+    this.$bus.$off("loginSuccessful");
+  },
+};
 </script>
